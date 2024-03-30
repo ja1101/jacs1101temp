a9de600d2ab58a6b97c47014814ab78b8645051e
Jose Alvarado <77592207+ja1101@users.noreply.github.com>
2024-03-29 19:16:16 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 import main
 
 def test_add1():
-    assert main.addThem(1, 2) == 3
+    assert main.addThem(1, 2, 3) == 6
 
 def test_add2():
-    assert main.addThem(2, 2) == 4
+    assert main.addThem(2, 2, 2) == 6
